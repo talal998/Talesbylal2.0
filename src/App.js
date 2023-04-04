95fce2222ce814cc8dcb7fcba667a64f3a8f3a31
talal998 <61560936+talal998@users.noreply.github.com>
2023-04-04 16:38:28 +0500

    Fixing bugs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import ReactGA from "react-ga";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-
 import Header from "./components/header/header.cmp";
 
 import HomePage from "./pages/homepage/homepage.cmp";
@@ -11,7 +10,6 @@ import PortfolioPage from "./pages/portfolio/portfolio.cmp";
 import AboutPage from "./pages/about/about.cmp";
 import ContactPage from "./pages/contact/contact.cmp";
 import PhotoLink from "./pages/photo-link/PhotoLink.cmp";
-
 
 class App extends React.Component {
   constructor() {
@@ -30,6 +28,18 @@ class App extends React.Component {
           <Header />
           <Switch>
             <Route
+              path={`${process.env.PUBLIC_URL}/about`}
+              component={AboutPage}
+            />
+            <Route
+              path={`${process.env.PUBLIC_URL}/contact`}
+              component={ContactPage}
+            />
+            <Route
+              path={`${process.env.PUBLIC_URL}/photos`}
+              component={PhotoLink}
+            />
+            <Route
               exact
               path="/"
               component={() => <HomePage handleClick={this.handleClick} />}
@@ -39,10 +49,11 @@ class App extends React.Component {
               path="/Talesbylal/"
               component={() => <HomePage handleClick={this.handleClick} />}
             />
-            <Route exact path="/Talesbylal/Portfolio/:title" component={PortfolioPage} />
-            <Route  path={`${process.env.PUBLIC_URL}/about`} component={AboutPage} />
-            <Route  path={`${process.env.PUBLIC_URL}/contact`} component={ContactPage} />
-            <Route  path={`${process.env.PUBLIC_URL}/photos`} component={PhotoLink} />
+            <Route
+              exact
+              path="/Talesbylal/Portfolio/:title"
+              component={PortfolioPage}
+            />
           </Switch>
         </div>
       </BrowserRouter>
